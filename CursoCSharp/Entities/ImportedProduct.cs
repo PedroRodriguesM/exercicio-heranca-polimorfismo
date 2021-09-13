57b2e5c97bd46c91600693505d77c1432738cb5b
Pedro Rodrigues M. da Silva <pedrorodriguesmoraesdasilva@outlook.com>
2021-09-13 15:12:44 -0300

    Herança e Polimorfismo

---
+++ CursoCSharp/Entities/ImportedProduct.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace CursoCSharp.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee)
+            : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " $ "
+                + TotalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + $" (Customs fee: $ {CustomsFee.ToString("f2", CultureInfo.InvariantCulture)})";
+        }
+
+        public double TotalPrice()
+        {
+            return Price += CustomsFee;
+        }
+    }
+}
---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using CursoCSharp.Entities;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> productList = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int numberProducts = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberProducts; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                string productType = Console.ReadLine();
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if (productType.ToUpper().Equals("I") || productType.ToUpper().Equals("IMPORTED"))
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine());
+                    productList.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (productType.ToUpper().Equals("C") || productType.ToUpper().Equals("COMMON"))
+                {
+                    productList.Add(new Product(name, price));
+                }
+                else if (productType.ToUpper().Equals("U") || productType.ToUpper().Equals("USED"))
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    productList.Add(new UsedProduct(name, price, manufactureDate));
+                }
+                else
+                {
+                    Console.WriteLine("This type of product doesn't exist!!!");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product prod in productList)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+        }
+    }
+}
